81ae8465ac575fdb7f2736b4fb52be57ec979b26
allybaubooa <allyhusseinbaubooa@gmail.com>
2026-08-07 13:04:25 +0400

    fixed requirements

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
+from flask import Flask, render_template, request, redirect, url_for
 import json
 import os
-from pathlib import Path
-
-from flask import Flask, redirect, render_template, request, url_for
-
-
-BASE_DIR = Path(__file__).resolve().parent
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def redirect_to_resume():
     return redirect(url_for('resume', lang='en'))
-
 
 @app.route('/resume')
 def resume():
@@ -21,16 +14,15 @@ def resume():
     if lang not in ['en', 'fr']:
         lang = 'en'
 
-    data_file = BASE_DIR / 'data' / f'resume_data_{lang}.json'
+    data_file = f'data/resume_data_{lang}.json'
 
-    if not data_file.exists():
+    if not os.path.exists(data_file):
         return f"Le fichier pour la langue '{lang}' est introuvable.", 404
 
-    with data_file.open('r', encoding='utf-8') as json_file:
+    with open(data_file, 'r', encoding='utf-8') as json_file:
         resume_data = json.load(json_file)
 
     return render_template('resume.html', resume_data=resume_data, lang=lang)
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', '8080')))
+    app.run(debug=True, port=8080)
